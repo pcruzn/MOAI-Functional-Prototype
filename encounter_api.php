2c72cf81bb42c6b5651f3863746cdf06e255fe4f
maliq <mmaliq@gmail.com>
2016-04-27 18:50:18 -0300

    Api to handle remove and select temporal encounter and redirect to list those

--- encounter_api.php
+++ encounter_api.php
@@ -1,6 +1,7 @@
 <?php
 include ("moai_db_connection.php");
 include ("models/encounterService.php");
+include ("models/scrapingService.php");
 
 function get_temporal_encounter_list()
 {
@@ -13,10 +14,15 @@ function get_temporal_encounter_list()
 }
 
 function select_temporal_encounter($selectedEncountersId){
-    
+    ScrapingService::setScrapedDataAsSelected($selectedEncountersId);
     return $selectedEncountersId;
 }
-$possible_url = array("get_temporal_encounter_list", "get_app", "Seleccionar");
+
+function drop_temporal_encounter($selectedEncountersId){
+    ScrapingService::setScrapedDataAsNotEligible($selectedEncountersId);
+    return $selectedEncountersId;
+}
+$possible_url = array("get_temporal_encounter_list", "get_app", "Seleccionar", "Eliminar");
 
 $value = "An error has occurred";
 
@@ -28,8 +34,19 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
             $value = get_temporal_encounter_list();
             break;
         case "Seleccionar":
-            if (isset($_GET["selectedEncounters"]))
+            if (isset($_GET["selectedEncounters"])) {
+                $value = is_array($_GET["selectedEncounters"]);
                 $value = select_temporal_encounter($_GET["selectedEncounters"]);
+                header("selectEncounter.php");
+            }
+            else
+                $value = "Missing argument";
+            break;
+        case "Eliminar":
+            if (isset($_GET["selectedEncounters"])) {
+                $value = is_array($_GET["selectedEncounters"]);
+                $value = drop_temporal_encounter($_GET["selectedEncounters"]);
+            }
             else
                 $value = "Missing argument";
             break;
@@ -43,4 +60,6 @@ if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
 }
 
 //return JSON array
-exit(json_encode($value));
+//exit(json_encode($value));
+echo "<meta http-equiv='refresh' content='0;url=temporaryEncounters.php'>";
+?>
