ba8dd2bd94bacecd4494b5d028f7825edb7ac63a
maliq <mmaliq@gmail.com>
2016-04-27 16:24:44 -0300

    flatui and angular files

---
+++ encounter_api.php
@@ -0,0 +1,46 @@
+<?php
+include ("moai_db_connection.php");
+include ("models/encounterService.php");
+
+function get_temporal_encounter_list()
+{
+    $rs = EncounterService::getAllTemporaryEncounters();
+    $rows = array();
+    while($r = mysql_fetch_assoc($rs)) {
+        $rows[] = $r;
+    }
+    return $rows;
+}
+
+function select_temporal_encounter($selectedEncountersId){
+    
+    return $selectedEncountersId;
+}
+$possible_url = array("get_temporal_encounter_list", "get_app", "Seleccionar");
+
+$value = "An error has occurred";
+
+if (isset($_GET["action"]) && in_array($_GET["action"], $possible_url))
+{
+    switch ($_GET["action"])
+    {
+        case "get_temporal_encounter_list":
+            $value = get_temporal_encounter_list();
+            break;
+        case "Seleccionar":
+            if (isset($_GET["selectedEncounters"]))
+                $value = select_temporal_encounter($_GET["selectedEncounters"]);
+            else
+                $value = "Missing argument";
+            break;
+        case "get_app":
+            if (isset($_GET["id"]))
+                $value = get_app_by_id($_GET["id"]);
+            else
+                $value = "Missing argument";
+            break;
+    }
+}
+
+//return JSON array
+exit(json_encode($value));
