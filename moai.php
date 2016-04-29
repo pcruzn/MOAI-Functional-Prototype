3b8d20640aed08c333127519a605b46d88ea4961
maliq <mmaliq@gmail.com>
2016-04-29 10:43:12 -0300

    updated moai.php with flat-ui layout

--- moai.php
+++ moai.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="flat-ui.css" />
-<title>MOAI</title>
-</head>
-
-
-<body>
+<?php
+  include("header.php")
+?>
 <h4>Bienvenido al software prototipo de MOAI</h4>
 <p>Seleccione un tipo de reporte:</p>
 
@@ -25,9 +18,5 @@
 <p><a href="encounterLocalizationRadarChart.php">Radar - Localización de Encuentros</a></p>
 <p><a href="encounterMicrolocalizationHistogramChart.php">Histograma - Microlocalización de encuentros</a></p>
 
-<footer>
-  <p align="right"><a href="index.php">Salir</a></p>
-</footer>
-
-</body>
-</html>
+<?php
+include ("footer.html");
