b40b14aaf19a6e07e0beac7bc0c1bff3d17484c8
maliq <mmaliq@gmail.com>
2016-04-30 22:22:28 -0300

    scrapingSourcesAdd updated to flat ui

--- scrapingSourceAdd.php
+++ scrapingSourceAdd.php
@@ -1,82 +1,60 @@
 <?php
 include ("moai_db_connection.php");
 include ("models/scrapingService.php");
+include ("header.php")
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" type="text/css" href="flat-ui.css" />
-<title>MOAI</title>
-</head>
+
+  <h3>Ingreso de fuente de 'scraping'</h3>
+  <div class="row">
+    <div class="span6">
+      <?php
+
+      if ($_GET['action'] == 1) {
+
+        // CAUTION: for now, has scraper is always 0!
+        if (ScrapingService::addScrapingSource($_POST['txtScrapingSourceName'], $_POST['txtSourceURL'], 0) == 1)
+          echo "<div class=\"alert alert-info moai-alert\">
+                                    <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>
+                                    Fuente agregada exitosamente...
+                                </div>";
 
 
-<body>
-<h3>Ingreso de fuente de 'scraping'</h3>
-<form id="scrapingSourceAddForm" name="scrapingSourceAddForm" method="post" action="scrapingSourceAdd.php?action=1">
-  <p>&nbsp;</p>
-  <table border="0" align="center">
-    <tr>
-      <td><p>&nbsp;</p>
-        <table width="481" border="1" align="center">
-          <tr>
-            <td width="144">Nombre:</td>
-            <td width="327" align="center"><label for="txtScrapingSourceName"></label>
-              <input name="txtScrapingSourceName" type="text" id="txtSourceName" size="50" maxlength="300" /></td>
-          </tr>
-          <tr>
-            <td>URL:</td>
-            <td align="center"><label for="textfield3"></label>
-              <input name="txtSourceURL" type="text" id="textfield3" size="50" maxlength="300" /></td>
-          </tr>
-          <tr>
-            <td>¿Tiene scraper?</td>
-            <td align="center"><table border="0">
-              <tr>
-                <td>Si</td>
-                <td><input type="radio" name="radioHasScraper" id="radio" value="scraperTrue" /></td>
-              </tr>
-              <tr>
-                <td>No</td>
-                <td><input name="radioHasScraper" type="radio" id="radio2" value="scraperNotTrue" checked="checked" /></td>
-              </tr>
-            </table>
-              
-                <label for="radioHasScraper"></label>
-                <label for="radioHasScraper"></label>
-              
-              </td>
-          </tr>
-        </table>
-        <p>&nbsp;</p>
-        <table border="0" align="center">
-          <tr>
-            <td><input type="submit" name="btnSubmit" id="btnSubmit" value="Guardar" />
-            <input type="reset" name="btnClear" id="btnClear" value="Reiniciar" /></td>
-          </tr>
-        </table></td>
-    </tr>
-  </table>
-</form>
-<p>&nbsp;</p>
+      }?>
+      <form class="form-horizontal" id="scrapingSourceAddForm" name="scrapingSourceAddForm" method="post" action="scrapingSourceAdd.php?action=1">
+        <div class="control-group small">
+          <label class="control-label" for="txtScrapingSourceName">Nombre</label>
+          <div class="controls">
+            <input name="txtScrapingSourceName" type="text" id="txtSourceName" class="span2" maxlength="300" />
+          </div>
+        </div>
+        <div class="control-group small">
+          <label class="control-label" for="textfield3">Url</label>
+          <div class="controls">
+            <input name="txtSourceURL" type="text" id="textfield3" class="span3" maxlength="300" />
+          </div>
+        </div>
+        <div class="control-group small">
+          <label class="control-label" for="textfield3">¿Tiene Scraper?</label>
+          <div class="controls">
+            <label class="radio">
+              <input type="radio" name="radioHasScraper" id="radio" value="scraperTrue" data-toggle="radio"/>Si
+            </label>
+
+            <label class="radio">
+              <input name="radioHasScraper" type="radio" id="radio2" value="scraperNotTrue" checked="checked" data-toggle="radio" />No
+            </label>
+          </div>
+        </div>
+        <div class="controls small">
+          <button type="submit" name="btnSubmit" id="btnSubmit" class="btn btn-primary">Guardar</button>
+          <button type="reset" name="btnClear" id="btnClear" class="btn btn-inverse">Reiniciar</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
 
 <?php
 
-if ($_GET['action'] == 1) {
-	
-	// CAUTION: for now, has scraper is always 0!
-	if (ScrapingService::addScrapingSource($_POST['txtScrapingSourceName'], $_POST['txtSourceURL'], 0) == 1)
-		echo "Fuente agregada exitosamente...";
-	
-		
-}
 
-
-?>
-
-<footer>
-  <p align="right"><a href="moai.php">Volver al inicio</a> <a href="index.php">Salir</a></p>
-</footer>
-
-</body>
-</html>
+include("footer.html");
