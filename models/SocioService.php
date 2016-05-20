6bf02991edf95f0ff34afd125fbb26c10e3fb49e
maliq <mmaliq@gmail.com>
2016-05-19 22:50:09 -0400

    fix include SocioService and removed echo and script code

--- models/SocioService.php
+++ models/SocioService.php
@@ -1,7 +1,7 @@
 <?php
 // delete this when used in the system
 // not required!
-include ("moai_db_connection.php");
+include ("../moai_db_connection.php");
 
 class SocioService {
 	// returns the count of a child attribute
@@ -13,7 +13,7 @@ class SocioService {
 		AND $tableAndColumnName.$tableAndColumnName = '$childAttribute'
 		GROUP BY '$childAttribute'";
 		
-		echo $childCountStatement;
+//		echo $childCountStatement . "\n";
 		
 		$result	 = 	mysql_query($childCountStatement) 
 					or die('Consulta fallida: ' . mysql_error());
@@ -25,25 +25,5 @@ class SocioService {
 		return $line[0];
 	}
 }
-
-// next lines are for testing purposes only!
-// you should include the following lines in your 'calling code/module'
-$xml = simplexml_load_file("groups.xml") or die("Error: Cannot create XML object");
-
-// this array will contain the count for each children
-$childrenCount = array();
-
-// catch all parents! (children() is a function of SimpleXML so it should not be confused with father and child in 
-// the domain context
-foreach($xml->children() as $father) { 
-	$tableAndColumnName = $father['tableAndColumnName'];
-    foreach ($father as $instance) {
-		// a father has children
-		// each child is here treated as an instance
-		array_push ($childrenCount, (int) SocioService::getCountByChildAttributeOnTable($instance, $tableAndColumnName));	
-	}	
-	// echo children count!
-	echo array_sum($childrenCount);
-} 
 
 ?>
